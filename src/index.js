dd695df5fcd9de9156ff0a03a9c0013d111cb37a
mazhoranta <mazhoranta@gmail.com>
2018-09-16 01:10:40 +0300

    write function getLoveTrianglesCount

--- src/index.js
+++ src/index.js
@@ -3,5 +3,24 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var loveTrianglesCount = 0;
+  var spicheneesCount = preferences.length;
+  if (spicheneesCount >= 3) {
+    for (var i = 0; i < spicheneesCount; i++) {
+      var firstSpichenee = preferences[i];
+      
+      if (firstSpichenee <= spicheneesCount) {
+        var secondSpichenee = preferences[firstSpichenee - 1];
+
+        if (secondSpichenee <= spicheneesCount && secondSpichenee != firstSpichenee) {
+          var thirdSpichenee = preferences[secondSpichenee - 1];
+
+          if ((thirdSpichenee - 1) == i && thirdSpichenee != secondSpichenee)
+            loveTrianglesCount++;
+        }
+      }
+    }
+  }
+
+  return loveTrianglesCount / 3;
 };
